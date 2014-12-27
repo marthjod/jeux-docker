222d5cacbe57fd2114f3168db50b4e14b8edaa94
marthjod <marthjod@gmail.com>
2014-12-27 11:19:36 +0100

    Update startup.sh

--- db/startup.sh
+++ db/startup.sh
@@ -3,9 +3,9 @@
 /usr/bin/mysqld_safe &
 sleep 3
 mysql -e "CREATE DATABASE jeuxdb;"
-mysql -e "CREATE USER 'jeuxdb_user'@'%' IDENTIFIED BY '1234';"
+mysql -e "CREATE USER 'jeuxdb_user'@'localhost' IDENTIFIED BY '1234';"
 # CREATE, ALTER for django
-mysql -e 'GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, INDEX ON `jeuxdb`.* TO `jeuxdb_user`@`%`;'
+mysql -e 'GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, INDEX ON `jeuxdb`.* TO `jeuxdb_user`@`localhost`;'
 mysql jeuxdb < /tmp/jeuxdb.sql
 
 killall mysqld
